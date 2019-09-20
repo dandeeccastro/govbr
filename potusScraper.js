8be8d8b09922f72978571753a55a3ef5c1d18770
Danilo Collares de Castro <dundeepessoal@gmail.com>
2019-09-20 20:09:21 -0300

    + file reader

--- potusScraper.js
+++ potusScraper.js
@@ -1,23 +1,25 @@
 /**
  * Bibliotecas necessárias para o Web Scraping
  */
+const readline = require('readline');
 const rp = require('request-promise');
 const $ = require('cheerio');
-const url = 'https://www.gov.br';
+const nj = require('numjs');
+const fs = require('fs');
 
 /**
  * Pega a url dada e retorna somente o domínio dela
  * @param {String} url 
  */
 function GetDomain(url) {
-  let strings = url.split('/');
-  let result = null;
-  for (let str of strings){
-	  if (str.includes('.gov.br')) {
-		result = str;
-		break;
-	  }
-  } return result;
+	let strings = url.split('/');
+	let result = null;
+	for (let str of strings) {
+		if (str.includes('.gov.br')) {
+			result = str;
+			break;
+		}
+	} return result;
 }
 
 /**
@@ -28,7 +30,7 @@ function GetDomain(url) {
  * @param {String} url 
  */
 function LinksToValues(url) {
-  return rp(url).then(function (html) {
+	return rp(url).then(function (html) {
 		let bundle = $('a', html);
 		let links = [];
 		for (let i = 0; i < bundle.length; i++) {
@@ -37,7 +39,7 @@ function LinksToValues(url) {
 				res = GetDomain(res);
 				if (res) {
 					// console.log(res)
-					if (!links.includes(res)){
+					if (!links.includes(res)) {
 						links.push(res);
 					}
 				}
@@ -46,6 +48,36 @@ function LinksToValues(url) {
 		return links;
 	})
 }
-LinksToValues(url).then(function (links) {
-	console.log(links, 1/links.length)
-}) ;
+
+function PageRank(sites) {
+	console.log(sites.length)
+	let result = nj.zeros([sites.length, sites.length]);
+	for (let i = 0; i < sites.length; i++) {
+		LinksToValues(sites[i]).then(function (links) {
+			let value = 1 / links.length;
+			for (let site in links) {
+				console.log("Value " + value + "added to position " + (i, sites.indexOf(site)))
+				result[i][sites.indexOf(site)] = value;
+			}
+		}
+		)
+	}
+}
+
+let reader = readline.createInterface({
+	input: fs.createReadStream('properFile.csv')
+});
+
+let url = [];
+function wtf() {
+	let list = []
+	reader.on('line', function (line) {
+		list.push(line);
+	}).on('close', function (line) {
+		console.log(list);
+		return list;
+	})
+}
+url = wtf()
+console.log(url)
+//PageRank(url);
