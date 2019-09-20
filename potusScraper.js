267f885d4af507669c206759c75de735e6b57a4f
Danilo Collares de Castro <dundeepessoal@gmail.com>
2019-09-20 18:41:57 -0300

    + funçao que retorna array de dominios unicos na base da Promise

--- potusScraper.js
+++ potusScraper.js
@@ -5,28 +5,47 @@ const rp = require('request-promise');
 const $ = require('cheerio');
 const url = 'https://www.gov.br';
 
+/**
+ * Pega a url dada e retorna somente o domínio dela
+ * @param {String} url 
+ */
 function GetDomain(url) {
-  let string = url;
-  url.includes("https") ? string = url.slice(8) : string = url.slice(7);
-  string = string.slice(0,string.indexOf('/'));
-  return string;
+  let strings = url.split('/');
+  let result = null;
+  for (let str of strings){
+	  if (str.includes('.gov.br')) {
+		result = str;
+		break;
+	  }
+  } return result;
 }
 
 /**
- * Pegando url e achando as tags a do html
+ * Pega a url dada, vê quais sites do governo ela aponta
+ * e nos retorna uma promise que tem a array dentro porque
+ * Javascript
+ * 
+ * @param {String} url 
  */
-rp(url).then(function (html) {
-  //success!
-  let bundle = $('a', html);
-  for (let i = 0; i < bundle.length; i++) {
-    if (bundle[i].attribs.href) {
-      let res = bundle[i].attribs.href;
-      if (res.includes(".gov.br")) {
-        console.log(GetDomain(res));
-      };
-    }
-  }
-})
-  .catch(function (err) {
-    //handle error
-  });
+function LinksToValues(url) {
+  return rp(url).then(function (html) {
+		let bundle = $('a', html);
+		let links = [];
+		for (let i = 0; i < bundle.length; i++) {
+			if (bundle[i].attribs.href) {
+				let res = bundle[i].attribs.href;
+				res = GetDomain(res);
+				if (res) {
+					// console.log(res)
+					if (!links.includes(res)){
+						links.push(res);
+					}
+				}
+			}
+		}
+		return links;
+	})
+}
+LinksToValues(url).then(function (links) {
+	console.log(links, 1/links.length)
+}) ;
